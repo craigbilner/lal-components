c396250a327555faa697393c2a82ff92048b28f1
Craig Bilner <craig.bilner@gmail.com>
2017-06-02 15:42:59 +0100

    Add card component

---
+++ packages/card/card.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  card: {
+    flexGrow: 1,
+    justifyContent: 'center'
+  },
+  title: {
+    fontSize: '1.25rem',
+    fontWeight: 'bold'
+  },
+  image: {
+    height: 400,
+    marginVertical: 10,
+    width: 400
+  }
+});
+
+const Card = ({ children }) => <View style={styles.card}>{children}</View>;
+const Title = ({ children }) => <Text style={styles.title}>{children}</Text>;
+const Photo = ({ uri }) => <Image source={{ uri }} style={styles.image}/>;
+
+export default ({ title, uri }) => (
+  <Card>
+    <Title>{title}</Title>
+    <Photo uri={uri}/>
+  </Card>
+);
